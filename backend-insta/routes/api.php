c27a2fa529c992b54eb019fcb0fce4b75be1a402
Rayan <rayanemad508@gmail.com>
2023-08-09 01:15:48 +0300

    BE : prepared apis

--- backend-insta/routes/api.php
+++ backend-insta/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
 
 
 Route::post('/signin', [AuthController::class, 'signIn']);
@@ -11,6 +12,14 @@ Route::post('/signup', [AuthController::class, 'signUp']);
 
 Route::group(["middleware" => "auth:api"], function(){
     $user = Auth::user(); 
+
+    Route::get('/users/search', [Controller::class, 'searchUsers']);
+    Route::get('/posts', [Controller::class, 'getPosts']);
+    Route::get('/posts/personal', [Controller::class, 'getPersonalPosts']);
+    Route::post('/follow', [Controller::class, 'addFollower']);
+    Route::post('/like', [Controller::class, 'addLike']);
+    Route::post('/posts', [Controller::class, 'addPost']);
+
   
 });
 
