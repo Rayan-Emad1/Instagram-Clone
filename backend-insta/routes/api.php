7980ebe4df909a9b88b69a7e0633dbe431119779
Rayan <rayanemad508@gmail.com>
2023-08-09 20:29:13 +0300

    BE : edited unauth path

--- backend-insta/routes/api.php
+++ backend-insta/routes/api.php
@@ -6,23 +6,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
 
 
+Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
 Route::post('/signin', [AuthController::class, 'signIn']);
 Route::post('/signup', [AuthController::class, 'signUp']);
 
 
 Route::group(["middleware" => "auth:api"], function(){
     $user = Auth::user(); 
-
     Route::get('/users/search', [Controller::class, 'searchUsers']);
     Route::get('/posts', [Controller::class, 'getPosts']);
     Route::get('/posts/personal', [Controller::class, 'getPersonalPosts']);
     Route::post('/follow', [Controller::class, 'addFollower']);
     Route::post('/like', [Controller::class, 'addLike']);
-    Route::post('/posts', [Controller::class, 'addPost']);
-
-  
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('/addpost', [Controller::class, 'addPost']);
 });
