ad4d62307093dcefea6e8ec68dc4b7c14a7b0051
Rayan <rayanemad508@gmail.com>
2023-08-10 01:30:22 +0300

    BE : fixed getPosts

--- backend-insta/app/Http/Controllers/Controller.php
+++ backend-insta/app/Http/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Post;
@@ -23,17 +23,31 @@ class Controller extends BaseController{
 
 
     public function getPosts(Request $request){
-        $posts = Post::all();
+        $user = Auth::user();
+
+        $followingIds = Follower::where('follower_id', $user->id)->pluck('following_id')->toArray();
+
+        $posts = Post::whereIn('user_id', $followingIds)->get();
+
+        $formattedPosts = [];
+        foreach ($posts as $post) {
+            $user = User::find($post->user_id);
+            $formattedPosts[] = [
+                'id' => $post->id,
+                'image_url' => $post->image_url,
+                'user_name' => $user->name,
+            ];
+        }
 
         return response()->json([
             'status' => 'Success',
-            'posts' => $posts,
+            'posts' => $formattedPosts,
         ]);
     }
 
     public function searchUsers(Request $request){
         $query = $request->name;
-        $users = User::where('name', 'like', "%$query%")->get();
+        $users = User::where('name', 'LIKE', "%$query%")->get();
 
         return response()->json([
             'status' => 'Success',
@@ -119,6 +133,14 @@ class Controller extends BaseController{
     public function addPost(Request $request){
         $user = Auth::user();
 
+        $validator = Validator::make($request->all(), [
+            'image_url' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
         $post = new Post();
         $post->user_id = $user->id;
         $post->image_url = $request->image_url;
@@ -131,16 +153,19 @@ class Controller extends BaseController{
     }
 
     public function getPostLikes(Request $request){
-
-        $request->validate([
-            'post_id' => 'required|numeric|exists:posts,id',
+        $validator = Validator::make($request->all(), [
+            'post_id' => 'required|exists:posts,id',
         ]);
-    
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
         $postId = $request->post_id;
-    
+
         $likedUserIds = Like::where('post_id', $postId)->pluck('user_id');
         $likedUsers = User::whereIn('id', $likedUserIds)->pluck('name');
-    
+
         return response()->json([
             'status' => 'success',
             'liked_users' => $likedUsers,
