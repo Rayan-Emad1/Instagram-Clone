ee6a67474276d2c523f697f2f8ae729e2c2fc7c8
Rayan <rayanemad508@gmail.com>
2023-08-09 20:31:15 +0300

    BE : removed foreign keys from tables

--- backend-insta/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend-insta/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
        Schema::create('users', function (Blueprint $table) {
         $table->id();
         $table->string('name');
-        $table->string('username');
         $table->string('email')->unique();
         $table->string('password');
         $table->timestamps();
@@ -29,3 +28,4 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
