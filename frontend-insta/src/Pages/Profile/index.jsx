e071b5decfbf1e61dc481e33f90edf900659c63d
Rayan <rayanemad508@gmail.com>
2023-08-10 21:53:10 +0300

    FE : added profile components

--- frontend-insta/src/Pages/Profile/index.jsx
+++ frontend-insta/src/Pages/Profile/index.jsx
@@ -1,9 +1,16 @@
-
-function Profile () {
-    return (
-        <h1>Profile</h1>
-    )
+import React from 'react';
+import Header from '../../Components/Header';
+import UserProfileInfo from '../../Components/UserProfileInfo'; // Adjust the path based on your folder structure
+import PostContainer from '../../Components/PostsContainer'
+ 
+const Profile = () => {
+  return (
+    <>
+      <Header />
+      <UserProfileInfo />
+      <PostContainer url = {'/posts/personal'} />
+    </>
+  );
 };
 
-
-export default Profile
+export default Profile;
