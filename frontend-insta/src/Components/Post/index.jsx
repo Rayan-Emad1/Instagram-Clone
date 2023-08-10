fe692227f0486db7b91ce702dbcecc4b0b4522d5
Rayan <rayanemad508@gmail.com>
2023-08-10 21:07:15 +0300

    FE : styled cards

--- frontend-insta/src/Components/Post/index.jsx
+++ frontend-insta/src/Components/Post/index.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+import './styles.css'
 
 const Post = ({ post, likePost}) => {
 
@@ -8,10 +9,13 @@ const Post = ({ post, likePost}) => {
   // //tried to reset name of button
   return (
     <div className="post-card">
-      <img src={post.image_url} alt={`Post by ${post.user_name}`} />
-      <span>{post.user_name}</span>
-      <span>Likes: {post.likes}</span>
-      <button onClick={() => likePost(post.id)}>Like</button>
+      <img className='post-image' src={post.image_url} alt={`Post by ${post.user_name}`} />
+      <h3>{post.user_name}</h3>
+      <div className='like-container'>
+        <span>Likes: {post.likes}</span>
+        <button className="like-button-post" onClick={() => likePost(post.id)}>Like</button>
+      </div>
+     
     </div>
   );
 };
