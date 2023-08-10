a4a74e74ef3dfacbb9e554e87da16418d07231fb
Rayan <rayanemad508@gmail.com>
2023-08-10 17:34:19 +0300

    FE : created search component

--- frontend-insta/src/Components/Header/index.jsx
+++ frontend-insta/src/Components/Header/index.jsx
@@ -1,44 +1,18 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SearchModal from '../SearchModel';
+import Searchbar from '../Searchbar';
 import './styles.css';
 
 const Header = () => {
-  const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  const token = localStorage.getItem('token');
-
-  const handleSearch = async () => {
-    try {    
-      if (!token) {
-        console.log('Token not found.');
-        return;
-      }
-  
-      const response = await fetch('http://127.0.0.1:8000/api/users/search', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ name: searchQuery }),
-      });
-  
-      const data = await response.json();
-      setSearchResults(data.users);
-      setShowModal(true);
-    } catch (error) {
-      console.log('Error:', error);
-    }
-  };
   
   const closeSearchModal = () => {
     setShowModal(false);
     setSearchResults([]);
   };
-
 
   const OpenProfile = () => {
     navigate('/profile')
@@ -49,14 +23,11 @@ const Header = () => {
     <header className="header-container">
 
       <div className="header-content">
-        <div className="logo">Instagram</div>
 
-        <div className="search-bar">
-          <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Search users..." />
-          <button onClick={handleSearch}>Search</button>
-        </div>
-        
+        <div className="logo">Instagram</div>
+        <Searchbar setSearchResults={setSearchResults} setShowModal={setShowModal} />
         <img onClick={OpenProfile} className="profile-image" src="https://w7.pngwing.com/pngs/128/223/png-transparent-user-person-profile-instagram-ui-colored-icon.png" />
+
       </div>
 
       {showModal && (<SearchModal searchResults={searchResults} closeModal={closeSearchModal}  />)}
--- frontend-insta/src/Pages/Main/index.jsx
+++ frontend-insta/src/Pages/Main/index.jsx
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../../Components/Header';
-import UserCard from '../../Components/UserCard';
 import Post from '../../Components/Post';
 
 const Main = () => {
-  const [users, setUsers] = useState([]);
+
   const [posts, setPosts] = useState([]);
 
-  // Fetch users and posts data from your API endpoints using useEffect
-
-  const followUser = async (userId) => {
-    // Implement API call to follow user with userId
-  };
-
   const likePost = async (postId) => {
-    // Implement API call to like post with postId
   };
 
   return (
     <div className="main-container">
+
       <Header />
+
       <div className="content">
-        <div className="user-search">
-          {users.map(user => (
-            <UserCard key={user.id} user={user} followUser={followUser} />
-          ))}
-        </div>
         <div className="posts-container">
           {posts.map(post => (
             <Post key={post.id} post={post} likePost={likePost} />
           ))}
+          
         </div>
       </div>
     </div>
